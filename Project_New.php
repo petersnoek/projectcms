e1a741a8c4fc7b06d4ebeb89adb17e6f6df02294
jelleloeve <jelle.loeve@hotmail.com>
2016-03-01 09:32:23 +0100

    Made an project_new.tpl for the page project_new.php, also made sure the correct code from project_new.php is in the new tpl file

--- Project_New.php
+++ Project_New.php
@@ -1,37 +1,11 @@
 <?php $pagetitle = 'Nieuw Project'; ?>
-<?php include 'inc/header.tpl'; ?>
-
-
-<form method="POST" >
-	<strong> Titel:</strong><br>
-		<input type="text" name="titel" value="">
-		<br><br>
-	<strong> Naam Opdrachtgever:</strong><br>
-		<input type="text" name="opdrachtgever" value="">
-		<br>
-	<strong> Bedrijf:</strong><br>
-		<input type="text" name="bedrijf" value="">
-		<br>
-	<strong> Telefoon:</strong><br>
-		<input type="text" name="telefoon" value="">
-		<br>
-	<strong> Email:</strong><br>
-		<input type="text" name="email" value="">
-		<br>
-	<strong> Adres:</strong><br>
-		<input type="text" name="adres" value="">
-		<br>
-	<strong> Plaats:</strong><br>
-		<input type="text" name="plaats" value="">
-		<br>
-	<strong> PC:</strong><br>
-		<input type="text" name="pc" value="">
-		<br>
-		<strong><p>Omschrijving:</p></strong>
-		<textarea name="omschrijving" cols="50" rows="3" id="detail"></textarea>
-		<br>
-     <input type="submit" name="submit" value="Submit">
-</form>
+<?php 	include("inc/class.TemplatePower.inc.php");
+		$tpl = new TemplatePower("tpl/project_new.tpl");
+		$tpl->assignInclude("header", "tpl/header.tpl");
+		$tpl->prepare();
+		$tpl->assign('pagetitle', 'Nieuw Project');
+		$tpl->printToScreen();
+ ?>
 
 <?php
 	if ( isset ($_POST['submit']))
