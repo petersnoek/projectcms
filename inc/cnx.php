ff96c527de3471164a5cfc6e529ba0324b4014fc
jelleloeve <jelle.loeve@hotmail.com>
2016-04-25 12:07:27 +0200

    Revert "Tried to make various changes such as  adding Sha512 which failed"
    This reverts commit ba949762a66e8c29bb5ef206aee962bb9cf5d811.

--- inc/cnx.php
+++ inc/cnx.php
@@ -1,16 +1,11 @@
 <?php
-    define("HOST", "localhost");     // The host you want to connect to.
-	define("USER", "cms");    // The database username. 
-	define("PASSWORD", "Studentje1");    // The database password. 
-	define("DATABASE", "projectcms");    // The database name.
- 
-	define("CAN_REGISTER", "any");
-	define("DEFAULT_ROLE", "member");
- 
-	define("SECURE", FALSE);    // FOR DEVELOPMENT ONLY!!!!
-		
+    $mysqlhost = 'localhost';
+    $mysqldb = 'projectcms';
+    $mysqluser = 'cms';
+    $mysqlpass = 'Studentje1';
+
     // create a connection to the database
-    $link = mysqli_connect(HOST, USER, PASSWORD, DATABASE);
+    $link = mysqli_connect($mysqlhost, $mysqluser, $mysqlpass, $mysqldb);
 
     // if something went wrong, display error information
     if (!$link) {
