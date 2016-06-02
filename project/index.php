5eb7ca5549aa070310a691a19c7684bee436be93
Peter Snoek <petersnoek@davinci.nl>
2016-06-02 11:42:44 +0200

    add new project folders for smarty

---
+++ project/index.php
@@ -0,0 +1,10 @@
+<?php
+// see: http://www.smarty.net/crash_course 
+
+
+// TODO: create smarty object
+
+// TODO: assign some values
+
+// TODO: display smarty template 'tpl/project.smarty.tpl'
+
---
+++ project/tpl/project.smarty.tpl
@@ -0,0 +1,1 @@
+<!-- see: http://www.smarty.net/crash_course -->
